1a340ce0f836611db1ae676a234b02755298f91e
Sam Mailand <sam.mailand@gmail.com>
2015-12-17 00:42:24 -0500

    Fix deletEvent
    deleting an event now also deletes all of the relevant time slots

--- server.js
+++ server.js
@@ -212,7 +212,11 @@ app.post('/denySignUp', function(req, res) {
 app.post('/deleteEvent', function(req, res) {
     var eventId = req.body.eventId;
     var userId = req.body.userId;
-    var query = client.query(("DELETE from events where event_id=" + eventId + " and event_owner_id=" + userId),
+
+    var queryString = "DELETE from events where event_id=" + eventId + " and event_owner_id=" + userId + ";"+
+        "DELETE from time_slot where event_id=" + eventId + ";"
+        console.log(queryString);
+    var query = client.query((queryString),
         function(err, result) {
             if (err) {
                 res.send(err);
